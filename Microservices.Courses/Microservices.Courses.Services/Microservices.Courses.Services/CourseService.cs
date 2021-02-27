e8437e937fe8a8a85ad0d538b8e3a652139d3704
Amir Hesari <amir.hesari1848@gmail.com>
2021-02-27 23:07:39 +0330

    *

--- Microservices.Courses/Microservices.Courses.Services/Microservices.Courses.Services/CourseService.cs
+++ Microservices.Courses/Microservices.Courses.Services/Microservices.Courses.Services/CourseService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microservices.Courses.DataLayer.Context;
+using Microservices.Courses.DataLayer.Models;
+using Microservices.Courses.Services.Dtos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +11,37 @@ namespace Microservices.Courses.Services
 {
     public class CourseService
     {
-        
+        private readonly CourseDbContext _db;
+
+        public CourseService(CourseDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<bool> CreateCourse(CreateCourseDto model)
+        {
+            try
+            {
+                Course course = new Course()
+                {
+                    CreateDate = DateTime.Now,
+                    Description = model.Description,
+                    EndDate = model.EndDate,
+                    Group = _db.Groups.Find(model.GroupId),
+                    GroupId = model.GroupId,
+                    Name = model.Name,
+                    StartDate = model.StartDate
+                };
+
+                await _db.Courses.AddAsync(course);
+                await _db.SaveChangesAsync();
+
+                return true;
+            }
+            catch (Exception exp)
+            {
+                return false;
+            }
+        }
     }
 }
---
+++ Microservices.Courses/Microservices.Courses.Services/Microservices.Courses.Services/Dtos/CreateCourseDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microservices.Courses.Services.Dtos
+{
+    public class CreateCourseDto
+    {
+        [MaxLength(100)]
+        [Required]
+        public string Name { get; set; }
+        [MaxLength(1500)]
+        public string Description { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime StartDate { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime EndDate { get; set; }
+        [Required]
+        public long GroupId { get; set; }
+    }
+}

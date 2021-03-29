dc781ee06416b9ddaaa622d0ce1c4b876d7c4f77
Amir Hesari <amir.hesari1848@gmail.com>
2021-03-29 17:22:17 +0430

    *

--- Microservices.Courses/Microservices.Courses.Services/Microservices.Courses.Services/Dtos/GroupDto.cs
+++ Microservices.Courses/Microservices.Courses.Services/Microservices.Courses.Services/Dtos/GroupDto.cs
@@ -11,5 +11,6 @@ namespace Microservices.Courses.Services.Dtos
         public long Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public string ParentGroup { get; set; }
     }
 }
